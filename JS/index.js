314bce2ec4413cf609cdac1b57be3aa4c134048e
ManuelaVillaM <mvillamu92@gmail.com>
2021-09-12 16:35:38 -0500

    Implement Gsap library animation

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,5 @@
 window.onload = () => {
+  init();
   const swiper = new Swiper('.swiper', {
     // speed: 400,
     // spaceBetween: 100,
@@ -20,6 +21,30 @@ window.onload = () => {
 
   setInterval(() => {
     swiperr.slideNext();
+    gsap.from(".box", {
+      duration: 2,
+      scale: 0.5, 
+      opacity: 0, 
+      delay: 0.5, 
+      stagger: 0.2,
+      ease: "elastic", 
+      force3D: true
+    });
   }, 2500);
+
+  function init () {
+    gsap.from(".box", {
+      duration: 2,
+      scale: 0.5, 
+      opacity: 0, 
+      delay: 0.5, 
+      stagger: 0.2,
+      ease: "elastic", 
+      force3D: true
+    });
+    gsap.from("#logo", {duration: 3, x: 300, opacity: 0, scale: 0.5});
+  }
+
+  
 }
  
